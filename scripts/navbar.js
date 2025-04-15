8312039eb86ae71e3f938c4e92c32595e9488bfc
Cole Shanks <43489492+coleshanks@users.noreply.github.com>
2025-04-15 15:02:50 +0900

    Update navbar.js
    Former-commit-id: 66ea6ee84e0707d1a788db11f5553635a7c3930b

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -7,13 +7,11 @@ let visibleNavbarHeight = document.querySelector('.navbar').offsetHeight;
 window.addEventListener("scroll", function() {
   let currentScroll = window.scrollY;
   let navbar = document.querySelector('.navbar');
-  let navbarHeight = navbar.offsetHeight;
-  let hiddenNavbarHeight = navbarHeight - visibleNavbarHeight;
-
+  
   if (currentScroll > lastScrollTop && currentScroll > scrollThreshold) {
     // Scroll Down - gradually move navbar up to escape
     let distanceToEscape = (currentScroll - scrollThreshold) / escapeSpeed;
-    navbar.style.top = `-${distanceToEscape}px`; // Move the navbar up to escape
+    navbar.style.top = `-${Math.min(distanceToEscape, navbarHeight)}px`;
   } else if (currentScroll < lastScrollTop || currentScroll <= scrollThreshold) {
     // Scroll Up or within threshold - show navbar
     navbar.style.top = '0';
