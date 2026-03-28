24664f7839744881e4d4c9707c99c6a24346d685
Cole Shanks <43489492+coleshanks@users.noreply.github.com>
2026-03-28 15:53:21 +0900

    poster added as default

--- scripts/video_poster.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if (window.innerWidth <= 400) {
-        var video = document.querySelector('.media-table video');
-        if (video) {
-            video.setAttribute('poster', '/assets/images/my pics/propbot-thumb.jpg');
-        }
-    }
-});
